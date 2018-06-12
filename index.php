e48210722d89cbbab36a6890165507630ee6a959
Stephane <smachetto@me.com>
2018-06-12 16:56:14 +0200

    Message From Code

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 phpinfo();
+
+function dosomething() 
+{
+    $data = 'Whazzup dude';
+    if(isset($data)) :
+        echo $data;
+        else :
+            echo 'nodata set';
+        endif;
+
+}
+
 ?>
